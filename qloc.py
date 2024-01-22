38fc4252eba7b5090be995bf247b898cec3e9295
Patrolin <patrik@balas.cz>
2024-01-22 13:11:00 +0100

    qloc try catch

--- qloc.py
+++ qloc.py
@@ -3,7 +3,7 @@ from os import walk
 from collections import Counter
 
 if __name__ == "__main__":
-  extension_to_loc = Counter()
+  loc_counters = Counter()
   rootPath = argv[1] if len(argv) > 1 else "."
   for (root, dirs, files) in walk(rootPath, topdown=True):
       root = root.replace("\\", "/")
@@ -11,11 +11,17 @@ if __name__ == "__main__":
       for file in files:
         path = f"{root}/{file}"
         extension = path.split('.')[-1]
+        acc_file_lines = 0
         with open(path, "r", encoding="utf8") as f:
-            line = f.readline()
-            while line:
-              if line.strip():
-                extension_to_loc[extension] += 1
+            try:
               line = f.readline()
-  for k, v in extension_to_loc.items():
+              while line:
+                if line.strip():
+                  acc_file_lines += 1
+                line = f.readline()
+            except (UnicodeDecodeError, PermissionError, OSError): # wtf
+                pass
+        if acc_file_lines:
+          loc_counters[extension] += acc_file_lines
+  for k, v in loc_counters.items():
     print(f".{k}: {v}")
