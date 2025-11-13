44a8b237ab411a11afdf13a38b0770b24f991fbb
Patrolin <patrik@balas.cz>
2025-11-13 21:58:59 +0100

    todo

---
+++ c99/lib/parse.h
@@ -0,0 +1,10 @@
+#include "definitions.h"
+
+// TODO: parse_i64(), ...
+// TOOD: parse_f64(), ...
+/*
+sign = "+" | "-"
+mantissa, exponent_offset = parse_int() "." parse_fraction()
+exponent = "e" parse_int()
+float = build_float(sign, mantissa, exponent_offset + exponent)
+*/
