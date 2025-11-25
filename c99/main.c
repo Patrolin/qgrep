6201cf6b01d5db31e5921dc5e8d06344792b6c86
Patrolin <patrik@balas.cz>
2025-11-25 12:34:26 +0100

    print short representations of floats via libc

--- c99/main.c
+++ c99/main.c
@@ -4,35 +4,69 @@
 // gdb foo-linux-x64
 #include "lib/all.h"
 #include <stdio.h>
+#include <string.h>
 _Check_return_ _ACRTIMP double __cdecl nan(_In_ char const* _X);
+
+/* NOTE: sign(1) + digits(17) + decimal_point(1) + signed_exponent(5) + null_terminator(1) */
+#define FORMAT_FLOAT_SIZE 1 + 17 + 1 + 5 + 1
+void format_float(f64 value) {
+  char original[FORMAT_FLOAT_SIZE];
+  int size = snprintf(original, sizeof(original), "%.17g", value);
+  /* TODO: if (isnan(value) || isinf(value)) return original; */
+  // find exponent
+  int exponent_index = 0;
+  while (exponent_index < size && original[exponent_index] != 'e') {
+    exponent_index++;
+  }
+  // round up and trucate
+  char shortened[FORMAT_FLOAT_SIZE];
+  int significand_end = exponent_index;
+  memcpy(shortened, original, FORMAT_FLOAT_SIZE);
+  while (significand_end >= 1) {
+    u8 carry = 1;
+    for (int j = significand_end - 2; j >= 0; j--) {
+      char c = shortened[j];
+      if (c == '.' || c == '-') continue;
+      u8 digit = (c - '0') + carry;
+      shortened[j] = '0' + (digit % 10);
+      carry = digit >= 10 ? 1 : 0;
+    }
+    memcpy(shortened + significand_end - 1, original + exponent_index, (size_t)(size - exponent_index + 1));
+    /* if valid transform, copy to original */
+    if (strtod(shortened, 0) == value) {
+      size--;
+      exponent_index--;
+      memcpy(original, shortened, (size_t)(size + 1));
+      significand_end--;
+      continue;
+    }
+    break;
+  }
+  // truncate
+  memcpy(shortened, original, (size_t)exponent_index);
+  while (significand_end >= 1) {
+    memcpy(shortened + significand_end - 1, original + exponent_index, (size_t)(size - exponent_index + 1));
+    /* if valid transform, keep in shortened */
+    if (strtod(shortened, 0) == value) {
+      significand_end--;
+      continue;
+    }
+    break;
+  }
+  // output
+  shortened[significand_end - 1] = original[significand_end - 1];
+  memcpy(shortened + significand_end, original + exponent_index, (size_t)(size - exponent_index + 1));
+  printf("%.17g -> %s\n", value, shortened);
+}
 
 void main_multicore(u32 t) {
   if (single_core(t)) {
-    f64 a = 2.0;
-    f64 b = 0.1;
-    f64 c = 0.1;
-    f64 result = fma(a, b, c);
-    assert(0.3 != 0.1 + 0.2);
-    assert(result == 0.1 + 0.2);
+    // intptr end;
+    // f64 x = parse_f64(string("0.5e1"), 0, &end);
+    // printfln1(string("end: %"), intptr, end);
 
-    intptr end;
-    f64 x = parse_f64(string("0.5e1"), 0, &end);
-    printfln1(string("end: %"), intptr, end);
-
-    printf("strtod(1.0): %g\n", strtod("1.0", 0));
-    printf("g: %g\n", 1.0 / 0.0);
-    printf("e: %g\n", nan("test"));
-    printf("g: %g\n", 1e6);
-    printf("g: %g\n", 1e5);
-
-    printf("g: %g\n", 0.1 + 0.2);               // 0.3
-    printf("g: %g\n", 0.3);                     // 0.3
-    printf("g: %.17g\n", 0.1 + 0.2);            // 0.30000000000000004
-    printf("g: %.17g\n", 0.3);                  // 0.29999999999999999
-    printf("g: %.17g\n", 0.30000000000000004);  // 0.30000000000000004
-    printf("g: %.17g\n", 0.29999999999999999);  // 0.29999999999999999
-
-    printf("g: %g\n", 1e-4);
-    printf("g: %g\n", 9e-5);
+    format_float(1.2e234);              // 1.2e+234
+    format_float(0.3);                  // 0.3
+    format_float(0.30000000000000004);  // 0.30000000000000004
   }
 }
